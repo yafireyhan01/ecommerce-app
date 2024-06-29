e30af6a81c7de2a184a637ee5bdc5c6b9dd59cdd
Annas Yafi Reyhan <yafireyhan01@gmail.com>
2024-06-30 01:57:27 +0700

    add db migrations for checkout

---
+++ migrations/000012_alter_checkout_add_order_date.up.sql
@@ -0,0 +1,2 @@
+alter table checkouts
+    add order_date timestamp default current_timestamp;
---
+++ migrations/000013_alter_checkout_drop_column.up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE checkouts
+    DROP COLUMN order_date,
+    DROP COLUMN qty,
+    DROP COLUMN total_price;
